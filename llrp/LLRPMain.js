44cbd42d9e0f8ffd486b05ef4575637c61655eb2
Wai Chun Leung <wc.leung@hotmail.com>
2022-11-17 16:56:58 +0100

    Added basic ROSpec configuration

--- llrp/LLRPMain.js
+++ llrp/LLRPMain.js
@@ -20,6 +20,7 @@ var LLRPMessage = require('./LLRPMessages.js');
 var decode = require('./decode.js');
 
 var net = require('net');
+const { rootCertificates } = require('tls');
 var EventEmitter = require('events').EventEmitter;
 
 var llrpMain = function (config) {
@@ -36,20 +37,9 @@ var llrpMain = function (config) {
 	var self = this;
 	var client = null;
 
-	//Defined message buffers. Brute force, I know I know.
-	var bSetReaderConfig = new Buffer('040300000010000000000000e2000580', 'hex');
-	var bEnableEventsAndReport = new Buffer('04400000000a00000000', 'hex');
-	var bAddRoSpec = new Buffer('04140000005d0000000000b1005300000001000000b2001200b300050000b60009000000000000b700180001000000b8000901000003e800ba000700010100ed001f01000000ee000bffc0015c0005c003ff000d000067ba0000008e01', 'hex');
-	var bEnableRoSpec = new Buffer('04180000000e0000000000000001', 'hex');
-	var bStartRoSpec = new Buffer('04160000000e0000000000000001', 'hex');
 	var bKeepaliveAck = new Buffer('04480000000a00000000', 'hex');
 	var bStopRoSpec = new Buffer(makeMessage(23, 0, [{ value: 1, bits: 32 }]), 'hex')
 	var bCloseConnection = new Buffer(makeMessage(14, 0), 'hex');
-	var bImpinjEnableExtensions = new Buffer(makeMessage(1023, 0, [
-		{ value: 25882, bits: 32 },
-		{ value: 21, bits: 8 },
-		{ value: 0, bits: 32 }
-	]), 'hex');
 
 	// ====================
 	// Public Methods
@@ -160,6 +150,8 @@ var llrpMain = function (config) {
 			let messageBuffer = undefined;
 			switch (messageC[message]) {
 				case messageC.ADD_ROSPEC:
+					messageBuffer = Buffer.from(makeMessage(20, 0, makeROSpec({ ROSpecID: 1, Priority: 0, CurrentState: 0 })), 'hex');
+					response = messageC.ADD_ROSPEC_RESPONSE;
 					break;
 				case messageC.DELETE_ROSPEC:
 					if (data.ROSpecID !== undefined) {
@@ -197,13 +189,18 @@ var llrpMain = function (config) {
 						response = messageC.SET_READER_CONFIG_RESPONSE;
 					}
 					break;
-				case message.CUSTOM_MESSAGE:
+				case messageC.CUSTOM_MESSAGE:
 					if (data !== null) {
-						messageBuffer = Buffer.from(makeMessage(1023, 0, data), 'hex');
-						response = messageC.CUSTOM_MESSAGE_RESPONSE;
+						messageBuffer = Buffer.from(makeMessage(1023, 0, [ 
+							{ value: data.vendorId, bits: 32},
+							{ value: data.subType, bits: 8 }, 
+							... data.data
+							]), 'hex');
+						response = messageC.CUSTOM_MESSAGE;
 					}
 					break;
 				default:
+					console.log("Wrong message");
 					break;
 			}
 
@@ -216,6 +213,13 @@ var llrpMain = function (config) {
 			self.on(response, (message) => {
 				resolve(message)
 			});
+
+			/*
+			setTimeout(() => {
+				console.log("TimeOut");
+				reject();
+			}, 10000);
+			*/
 		});
 
 		return promise;
@@ -226,7 +230,115 @@ var llrpMain = function (config) {
 	// ====================
 
 	/**
-	 * Helper function to make binary encodings
+	 * Function to make to ROspec
+	 * @param {Object} ROSpec RoSpec object
+	 * 
+	 */
+	function makeROSpec(ROSpec) {
+		const rsv = { value: 0, bits: 6 };
+
+		//ROSpec
+		const ROSpecType = { value: 177, bits: 10 };
+
+		const ROSpecId = { value: ROSpec.ROSpecID, bits: 32 };
+		const ROSpecPriority = { value: ROSpec.Priority, bits: 8 };
+		const ROSpecCurrentState = { value: ROSpec.CurrentState, bits: 8 };
+
+		//ROBoundary
+		const ROBoundaryType = { value: 178, bits: 10 };
+
+		//ROSpecStartTrigger
+		const ROSpecStartType = { value: 179, bits: 10 };
+		const ROSpecStartTriggerType = { value: 0, bits: 8 };
+
+		//ROSpecStopTrigger
+		const ROSpecStopType = { value: 182, bits: 10 };
+		const ROSpecStopTriggerType = { value: 0, bits: 8 };
+		const ROSpecStopTriggerDuration = { value: 0, bits: 32 };
+
+		//AISpec
+		const AISpecType = { value: 183, bits: 10 };
+		const AntennaCount = { value: 1, bits: 16 };
+		const AntennaId = { value: 0, bits: 16 };
+
+		const AISpecStopType = { value: 184, bits: 10 };
+		const AISpecStopTriggerType = { value: 0, bits: 8 };
+		const AISpecStopTriggerDuration = { value: 0, bits: 32 };
+
+		//Tagobservation
+		const TagObservationType = { value: 185, bits: 10 };
+		const TagObservationTriggerType = { value: 0, bits: 8 };
+		const TagObservationTriggerRsv = { value: 0, bits: 8 };
+		const TagObservationTriggerNumber = { value: 1, bits: 16 };
+		const TagObservationTriggerAttemps = { value: 0 , bits: 16 };
+		const TagObservationTriggerT = { value: 0, bits: 16 };
+		const TagObservationTriggerTimeout = { value: 0, bits: 32 };
+
+		//InventoryParameterSpec
+		const InventoryParameterSpecType = { value: 186, bits: 10 };
+		const InventoryParameterSpecId = { value: 1, bits: 16 };
+		const InventoryParameterSpecProtocolId = { value: 1, bits: 8 };
+		
+		//AntennaConfiguration
+		const AntennaConfigurationType = { value: 222, bits: 10 };
+		const AntennaConfigurationId = { value: 0, bits: 16 };
+		
+		//RFTransmitterSettings
+		const RFTransmitterSettingsType = { value: 224, bits: 10 };
+		const RFTransmitterSettingsHopTableId = { value: 0, bits: 16 };
+		const RFTransmitterSettingsChannelIndex = { value: 1, bits: 16 };
+		const RFTransmitterSettingsTransmit = { value: 10, bits: 16 };
+
+		//Data Arrays
+		const RFTransmitterSettings = [rsv, RFTransmitterSettingsType, { value: 0, bits: 16 }, RFTransmitterSettingsHopTableId, RFTransmitterSettingsChannelIndex, RFTransmitterSettingsTransmit]
+		RFTransmitterSettings[2].value = countBytes(RFTransmitterSettings);
+
+		const AntennaConfiguration = [rsv, AntennaConfigurationType, { value: 0, bits: 16 }, AntennaConfigurationId, ...RFTransmitterSettings];
+		AntennaConfiguration[2].value = countBytes(AntennaConfiguration);
+
+		const InventoryParameterSpec = [rsv, InventoryParameterSpecType, { value: 0, bits: 16 }, InventoryParameterSpecId, InventoryParameterSpecProtocolId, ...AntennaConfiguration];
+		InventoryParameterSpec[2].value = countBytes(InventoryParameterSpec);
+		
+		const Tagobservation = [rsv, TagObservationType, { value: 0, bits: 16 }, TagObservationTriggerType, TagObservationTriggerRsv, TagObservationTriggerNumber, TagObservationTriggerAttemps, TagObservationTriggerT, TagObservationTriggerTimeout];
+		Tagobservation[2].value = countBytes(Tagobservation);
+
+		const AISpecStop = [rsv, AISpecStopType, { value: 0, bits: 16}, AISpecStopTriggerType, AISpecStopTriggerDuration]//, ...Tagobservation];
+		AISpecStop[2].value = countBytes(AISpecStop);
+
+		const AISpec = [rsv, AISpecType, { value: 0, bits: 16 }, AntennaCount, AntennaId, ...AISpecStop, ...InventoryParameterSpec];
+		AISpec[2].value = countBytes(AISpec);
+
+		const ROSpecStopTrigger = [rsv, ROSpecStopType, { value: 0, bits: 16 }, ROSpecStopTriggerType, ROSpecStopTriggerDuration];
+		ROSpecStopTrigger[2].value = countBytes(ROSpecStopTrigger);
+
+		const ROSpecStartTrigger = [rsv, ROSpecStartType, { value: 0, bits: 16 }, ROSpecStartTriggerType];
+		ROSpecStartTrigger[2].value = countBytes(ROSpecStartTrigger);
+
+		const ROBoundary = [rsv, ROBoundaryType, { value: 0, bits: 16 }, ...ROSpecStartTrigger, ...ROSpecStopTrigger];
+		ROBoundary[2].value = countBytes(ROBoundary);
+
+		const ROSpecData = [rsv, ROSpecType, { value: 0, bits: 16 }, ROSpecId, ROSpecPriority, ROSpecCurrentState, ...ROBoundary, ...AISpec];
+		ROSpecData[2].value = countBytes(ROSpecData);
+
+		return ROSpecData;
+	}
+
+	/**
+	 * Returns number of bytes in data
+	 * 
+	 * @param {Object[]} array Array of data object { value: 1, bits: 16 }
+	 * @returns 
+	 */
+	function countBytes(array) {
+		let length = 0;
+		for (let i = 0; i < array.length; i++) {
+			length += array[i].bits;
+		}
+		return parseInt(length / 8);
+	}
+
+	/**
+	 * Function to make binary encodings
 	 * 
 	 * @example <caption>Example of usage</caption>
 	 * const hex = makeMessage(21, 0, [{ value: 1, bits: 32 }])
@@ -259,14 +371,22 @@ var llrpMain = function (config) {
 
 		const message = BigInt('0b' + rsvd + ver + type + length + id + data).toString(16);
 
+		console.log('0' + message);
+
 		return '0' + message;
 	}
 
 	function handleMessage(message) {
-		var parametersKeyValue = decode.parameter(message.getParameter());
-		if (log) {
-			console.log(parametersKeyValue);
+		const parametersKeyValue = decode.parameter(message.getParameter());
+		if (parametersKeyValue.length !== undefined) {
+			for (let i = 0; i < parametersKeyValue.length; i++) {
+				if (parametersKeyValue[i].typeName === 'LLRPStatus') {
+					parametersKeyValue[i].status = parametersKeyValue[i].value[1];
+					parametersKeyValue[i].desc = parametersKeyValue[i].value.toString();
+				}
+			}
 		}
+		console.log(parametersKeyValue);
 	}
 
 	function handleReaderNotification(message) {
@@ -357,19 +477,6 @@ var llrpMain = function (config) {
 		//message type resides on the first 2 bits of the first octet
 		//and 8 bits of the second octet.
 		return (data[0] & 3) << 8 | data[1];
-	}
-
-	/**
-	 * Sends a START_ROSPEC message if it has not been sent.
-	 *
-	 * @return {Int} returns the length written or false if there was an error writing.
-	 */
-	function sendStartROSpec() {
-		//START_ROSPEC has not been sent.
-		if (!isStartROSpecSent) {
-			isStartROSpecSent = true; //change state of flag.
-			writeMessage(client, bStartRoSpec); //send START_ROSPEC
-		}
 	}
 
 	/**
